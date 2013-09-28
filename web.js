c05fe412cad7d5239ea3dcdc7cf3a2e622274af8
tjwebb <travis@traviswebb.com>
2013-09-28 19:22:29 -0400

    added cors to quote route

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var util   = require('util'),
   express  = require('express'),
   logistify = require('./routes/logistify'),
-  color    = require('cli-color');
+  color    = require('cli-color'),
+  cors     = require('cors');
 
 process.on('uncaughtException', function(err) {
   util.log(color.red('[main] uncaught exception: '+ err.stack));
@@ -12,7 +13,7 @@ app.use(express.logger());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 
-app.get ('/quote/:scac', logistify.quote);
+app.get ('/quote/:scac', cors(), logistify.quote);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
