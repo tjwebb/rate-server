0effeb7202e8d1a49132d2d7cfa6b328e007c399
tjwebb <travis@traviswebb.com>
2013-09-29 14:45:05 -0400

    made fake quotes slightly more believable

--- carrier-profiles/cnwy.js
+++ carrier-profiles/cnwy.js
@@ -11,7 +11,7 @@
   };
 
   module.exports.getQuote = function (query) {
-    var quote = ((Math.random() * 50) + 100).toFixed(2);
+    var quote = ((Math.random() * 400) + 500).toFixed(2);
 
     module.exports.emit('quoteReceived', quote);
   };
