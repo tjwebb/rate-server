5dfbe625b7e584f291affdd3f2717bc9f07f04a8
tjwebb <travis@traviswebb.com>
2013-09-29 13:07:47 -0400

    added cors, try 2

--- web.js
+++ web.js
@@ -1,8 +1,7 @@
 var util   = require('util'),
   express  = require('express'),
   logistify = require('./routes/logistify'),
-  color    = require('cli-color'),
-  cors     = require('cors');
+  color    = require('cli-color');
 
 process.on('uncaughtException', function(err) {
   util.log(color.red('[main] uncaught exception: '+ err.stack));
@@ -13,7 +12,15 @@ app.use(express.logger());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 
-app.get ('/quote/:scac', cors(), logistify.quote);
+// enable cors
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+// routes
+app.get ('/quote/:scac', logistify.quote);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
